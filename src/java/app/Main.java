932bd34cf2a084f0603a9b00975e4a4ba135575e
snow_penguin <penguinsneg13@gmail.com>
2024-04-23 21:52:40 +0300

    Made the code more readable

--- src/java/app/Main.java
+++ src/java/app/Main.java
@@ -6,13 +6,14 @@ public class Main {
         float sBD1 = 2430.68f;
         int No2Days = 7;
         float sBD2 = 1498.12f;
-
+        double salesOne = No1Days * sBD1;
+        double salestwo = No2Days * sBD2;
 
         System.out.printf("Product No 1: smartphone,%n" +
                 "total sales for %x days is EUR %g,%n" +
                 "sales by day is EUR %g,%n" +
                 "Product No 2: laptop,%n" +
                 "total sales for %x days is EUR %g,%n" +
-                "sales by day is EUR %g.", No1Days, No1Days * sBD1, sBD1, No2Days, No2Days * sBD2, sBD2);
+                "sales by day is EUR %g.", No1Days, salesOne, sBD1, No2Days, salestwo, sBD2);
     }
 }
