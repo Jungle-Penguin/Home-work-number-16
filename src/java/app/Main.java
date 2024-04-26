44f696a67d7451258aecf85e37bfdbf76857de6b
snow_penguin <penguinsneg13@gmail.com>
2024-04-26 21:44:56 +0300

    did a home work number five

--- src/java/app/Main.java
+++ src/java/app/Main.java
@@ -1,19 +1,22 @@
 package app;
-
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        int No1Days = 5;
-        float sBD1 = 2430.68f;
-        int No2Days = 7;
-        float sBD2 = 1498.12f;
-        double salesOne = No1Days * sBD1;
-        double salestwo = No2Days * sBD2;
 
-        System.out.printf("Product No 1: smartphone,%n" +
-                "total sales for %x days is EUR %g,%n" +
-                "sales by day is EUR %g,%n" +
-                "Product No 2: laptop,%n" +
-                "total sales for %x days is EUR %g,%n" +
-                "sales by day is EUR %g.", No1Days, salesOne, sBD1, No2Days, salestwo, sBD2);
+    public static void main(String[] args) {
+
+        System.out.println("write your slary: ");
+        Scanner scanner = new Scanner(System.in);
+        double userS = scanner.nextInt();
+
+        if(userS < 0){
+            System.out.println("find a job man...");
+        } else if (userS <= 10000) {
+            System.out.println("Greetings your taxes is : " + userS / 100 * 2.5);
+        } else if (userS <= 25000){
+            System.out.println("Greetings your taxes is : " + userS / 100 * 4.3);
+        } else{
+            System.out.println("Greetings your taxes is : " + userS / 100 * 6.7);
+        }
+
     }
 }
