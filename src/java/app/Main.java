51cff3cfa3474e24c5fd56f9f0356db811575874
snow_penguin <penguinsneg13@gmail.com>
2024-05-01 00:10:47 +0300

    did a home work number six

--- src/java/app/Main.java
+++ src/java/app/Main.java
@@ -1,22 +1,25 @@
 package app;
-import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args) {
 
-        System.out.println("write your slary: ");
-        Scanner scanner = new Scanner(System.in);
-        double userS = scanner.nextInt();
+        public static void main(String[] args){
 
-        if(userS < 0){
-            System.out.println("find a job man...");
-        } else if (userS <= 10000) {
-            System.out.println("Greetings your taxes is : " + userS / 100 * 2.5);
-        } else if (userS <= 25000){
-            System.out.println("Greetings your taxes is : " + userS / 100 * 4.3);
-        } else{
-            System.out.println("Greetings your taxes is : " + userS / 100 * 6.7);
+
+
+            int x = 0;
+
+
+            int y = 0;
+
+
+            for (int num = 1; num <= 6; num++){
+                    y += num;
+                    x += num;
+
+                    System.out.println(num + ") Num is " + num + ", sum is: "+ y);
+
+                }
+
+            System.out.println("Sum if numbers is " +  x);
         }
-
     }
-}
