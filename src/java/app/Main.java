1df69c77842c8140877d1125fc40fafeaacadc73
snow_penguin <penguinsneg13@gmail.com>
2024-05-17 03:12:22 +0300

    did my home work number ten!

--- src/java/app/Main.java
+++ src/java/app/Main.java
@@ -1,129 +1,110 @@
 package app;
-
 import java.util.Random;
+import java.util.Scanner;
+
+/*
+
+Напишіть метод, який приймає ціле число як аргумент і виводить його квадрат.
+Напишіть метод, який приймає два аргументи типу double - радіус та висоту - і повертає об'єм циліндра. Виведіть об'єм циліндра на екран.
+Напишіть метод, який приймає масив цілих чисел та обчислює і повертає суму всіх елементів масиву.
+Напишіть метод, який приймає рядок (String) як аргумент та повертає новий рядок, який складається з букв цього рядка у зворотньому порядку.
+Напишіть метод, який приймає два цілих числа, a та b, і повертає результат a^b (a підняте до степеня b).
+Напишіть метод, який приймає параметри: ціле число n і рядок text. Метод повинен вивести текстовий рядок text n рази, розділяючи кожен рядок символом переносу.
+
+*/
 
 public class Main {
 
-    public static void main(String[] args) {
-//        int[][] arr = {{1, 15, 14, 4}, {12, 6, 7, 9}, {8, 10, 11, 5}, {13, 3, 2, 16}}; Проверка на магический кубиk
-        int[][] arr = new int[4][4];
+    public static int texting(String text, int n) throws InterruptedException{
 
-        int zeZero = 0;
-        int onZero = 0;
-        int twZero = 0;
-        int thZero = 0;
-        int zeroZe = 0;
-        int zeroOn = 0;
-        int zeroTw = 0;
-        int zeroTh = 0;
-        int diagonalR = 0;
-        int diagonalL = 0;
-        int pairSum = 0;
-        int nonePairSum = 0;
-        double multiplyPair = 1;
-        double multiplyNonePair = 1;
-        
+        for (int i = 0; i != n; i++) {
+            Thread.sleep(300);
+            System.out.println(text + "\n");
+        }
+        return -1;
+    }
+
+    public static double degree(double a, int b){
+        double p = a;
+        for (int i = 1; i != b; i++) {
+
+            a *= p;
+        }
+        return a;
+    }
+
+
+    public static String conversely(String l){
+        return new StringBuilder(l).reverse().toString();
+    }
+
+
+    public static double aDouble(int x){
+        return x*x;
+    }
+
+    public static int arraySum(int[] x){
+
+        int y = 0;
+        for (int i = 0; i < x.length; i++) {
+
+            y += x[i];
+        }
+        return y;
+    }
+
+    public static double cylinder(double h, double r){
+        return Math.PI * r * r * h;
+    }
+
+
+    public static void main(String[] args) throws InterruptedException{
+        Scanner sc = new Scanner(System.in);
         Random random = new Random();
-        for (int i = 0; i < arr.length; i++) {
 
-            for (int j = 0; j < arr[i].length; j++) {
+        int[] ints = new int[5];
 
-                arr[i][j] = random.nextInt(1, 51);
-
-            }
+        for (int i = 0; i < 5; i++) {
+            ints[i] = random.nextInt(0,25);
         }
 
-        for (int[] anInt : arr) {
-            for (int i : anInt) {
-                System.out.printf("[%s] ", i);
-            }
-            System.out.println();
+        System.out.println("write your number and i double it >:3 : ");
+        int x = sc.nextInt();
+        System.out.println(aDouble(x));
+
+
+        System.out.println("write height of cylinder : ");
+        double h = sc.nextInt();
+        System.out.println("write cylinder Radius : ");
+        double r = sc.nextInt();
+
+        System.out.println(cylinder(h, r));
+
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("[%s]", ints[i]);
         }
 
+        System.out.println("\n" + arraySum(ints));
+
+        System.out.println("What is your name?..");
+        String l = sc.next();
+        System.out.println(conversely(l));
 
 
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-//                System.out.println(i + "." + j);
-                if (i == j) {
-                    diagonalL += arr[i][j];
-                }
-
-                switch (j) {
-                    case 0 -> zeroZe += arr[i][j];
-                    case 1 -> zeroOn += arr[i][j];
-                    case 2 -> zeroTw += arr[i][j];
-                    case 3 -> zeroTh += arr[i][j];
-                }
-                switch (i) {
-                    case 0 -> zeZero += arr[i][j];
-                    case 1 -> onZero += arr[i][j];
-                    case 2 -> twZero += arr[i][j];
-                    case 3 -> thZero += arr[i][j];
-                }
-            }
-
-//            System.out.println(zeroZe + " " + zeroOn + " " + zeroTw + " " + zeroTh); System.out.println(diagonalL);
-        }
-
-
-        int y = 3;
-        int x = 0;
-        while (y != -1) {
-            diagonalR += arr[y][x];
-            y--;
-            x++;
-        }
+        System.out.println("write the number you want to add to the degree: ");
+        int a = sc.nextInt();
+        System.out.println("write down what degree you want the number to go to: ");
+        int b = sc.nextInt();
+        System.out.println(degree(a,b));
 
 
 
-        for (int i = 0; i < 4; i += 2) {
-            for (int j = 0; j < 4; j++) {
-//                System.out.printf("%s + ", arr[i][j]); output code for debug + (pair)
-                pairSum += arr[i][j];
-            }
-
-        }
-
-        for (int i = 0; i < 4; i += 2) {
-
-            for (int j = 0; j < 4; j++) {
-//                System.out.printf("%s * ", arr[i][j]); output code for debug * (pair)
-                multiplyPair *= arr[i][j];
-            }
-            System.out.println();
-        }
-        for (int i = 1; i < 4; i += 2) {
-
-            for (int j = 0; j < 4; j++) {
-//                System.out.printf("%s * ", arr[i][j]); output code for debug * (not pair)
-                multiplyNonePair *= arr[i][j];
-            }
-            System.out.println();
-        }
-
-        for (int i = 1; i < 4; i += 2) {
-            for (int j = 0; j < 4; j++) {
-//                System.out.printf("%s + ", arr[i][j]); output code for debug + (not pair)
-                nonePairSum += arr[i][j];
-            }
-        }
-
-        int zeOne = zeZero + onZero + twZero + thZero + zeroZe + zeroOn + zeroTw + zeroTh;                       // для проверки на магический кубик
-        int dio = diagonalL + diagonalR + diagonalL + diagonalR + diagonalL + diagonalR + diagonalL + diagonalR; // для проверки на магический кубик
-
-        System.out.println("sum of paired elements on matrix(4x4) and with index 0 2: " + pairSum);
-        System.out.println("Sum of not paired elements on matrix(4x4) and with index 1 3: " + nonePairSum);
-        System.out.println("multiply of paired elements on matrix(4x4) and with index 0 2: " + multiplyPair);
-        System.out.println("multiply of not paired elements on matrix(4x4) and with index 1 3: " + multiplyNonePair);
-
-        if (zeOne == dio) {                              // проверка на магический кубик
-            System.out.println("this is MAGIC CUBE");    // проверка на магический кубик
-        }else{                                           // проверка на магический кубик
-            System.out.println("this is NOT MAGIC CUBE");// проверка на магический кубик
-        }
-
+        System.out.println("Can you repeat please, what is your name?..");
+        String text = sc.next();
+        System.out.println("how many times I have to repeat your name to remember it?.. (write integer): ");
+        int n = sc.nextInt();
+        texting(text, n);
     }
 }
 
