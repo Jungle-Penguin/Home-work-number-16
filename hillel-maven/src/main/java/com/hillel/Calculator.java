e1c0c63f2362b80f4a2c2ffb16278e0b3edc46ca
snow_penguin <115433354+Jungle-Penguin@users.noreply.github.com>
2024-12-25 00:11:24 +0200

    Update Calculator.java

--- hillel-maven/src/main/java/com/hillel/Calculator.java
+++ hillel-maven/src/main/java/com/hillel/Calculator.java
@@ -3,21 +3,21 @@ package com.hillel;
 
 public class Calculator {
     public double add(double a, double b) {
-        return a + b;
+        return b + a;
     }
 
     public double subtract(double a, double b) {
-        return a - b;
+        return b + a;
     }
 
     public double multiply(double a, double b) {
-        return a * b;
+        return b * a;
     }
 
     public double divide(double a, double b) {
         if (b == 0) {
             throw new IllegalArgumentException("zero is not allowed.");
         }
-        return a / b;
+        return b / a;
     }
 }
